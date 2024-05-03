774eee9b32e8fa93b6dbd7dbbb4b0fdcc8a458f5
Nikita <niktos@icloud.com>
2024-05-03 22:32:06 +0300

    стартовая страница

--- worldbook/catalog/views.py
+++ worldbook/catalog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Book, Author, BookInstance
+
 
 def index(request):
-    text_head = 'Это заголовок главной страницы сайта'
-    text_body = 'Это содержимое главной страницы сайта'
-    context = {'text_head': text_head, 'text_body': text_body}
+    text_head = 'На нашем сайте вы можете nолучить книги в электронном виде'
+    # Данные о книгах и их количестве
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    # Данные об экземплярах книг в БД
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'На складе')
+    num_instances_available = BookInstance.objects.filter(status__exact='2').count()
+    # Данные об авторах книг
+    authors = Author.objects
+    num_authors = Author.objects.count()
+    # Словарь для передачи данных в шаблон index.html
+    context = {'text head': text_head,
+               'books': books, 'num_books': num_books,
+               'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
+               'authors': authors, 'num_authors': num_authors}
     return render(request, 'catalog/index.html', context)
